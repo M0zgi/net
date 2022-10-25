4952107c03538945e022494d19d3060978d29201
Aleksandr <mozg.com@gmail.com>
2022-10-25 22:15:42 +0300

    add DB

---
+++ GetStreet/Lib/Migrations/20221025191201_firstCreate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lib.Migrations
+{
+    public partial class firstCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ZipCodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ZipCodes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Streets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StreetName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    ZipCodeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Streets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Streets_ZipCodes_ZipCodeId",
+                        column: x => x.ZipCodeId,
+                        principalTable: "ZipCodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Streets_ZipCodeId",
+                table: "Streets",
+                column: "ZipCodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ZipCodes_Code",
+                table: "ZipCodes",
+                column: "Code",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Streets");
+
+            migrationBuilder.DropTable(
+                name: "ZipCodes");
+        }
+    }
+}
